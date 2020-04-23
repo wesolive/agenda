b4ac4be0df2965e55a1dcb1c97f04b15403e6d4e
wesolive <63858114+wesolive@users.noreply.github.com>
2020-04-23 13:10:09 -0300

    criando tela de registro da agenda

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from core.models import Evento
+
+# Create your views here.
+def index(request):
+    return redirect('/agenda/')
+
+
+def listaEventos(request):
+    usuario = request.user
+    evento = Evento.objects.filter(usuario = usuario)
+    dados = {'eventos':evento}
+    return render(request,'agenda.html',dados)
