8b08d4e4e06daa847f153d31bde5d3c559b2390d
wesolive <63858114+wesolive@users.noreply.github.com>
2020-04-24 11:57:07 -0300

    criando tela de loginpara agenda

--- Agenda/urls.py
+++ Agenda/urls.py
@@ -20,5 +20,8 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.listaEventos),
-    path('', views.index)
+    path('', views.index),
+    path('login/', views.login_user),
+    path('login/submit',views.submit_login),
+    path('logout/', views.logout_user)
 ]
